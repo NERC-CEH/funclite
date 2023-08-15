4c845aa31858489f8d1658b96e99f90ebd93930f
Graham Monkman <gmonkman@mistymountains.biz>
2023-08-15 16:12:33 +0100

    tweaks

--- mixins.py
+++ mixins.py
@@ -1,9 +1,9 @@
 """mixins"""
+from abc import ABCMeta as _ABCMeta
 
-
-class MixinNameSpace:
-    def __init__(self, *args, **kwargs):
+class MixinNameSpace(_ABCMeta):
+    def __init__(self, *args, **kwargs):  # noqa
         raise NotImplementedError('This class is intended as a namespace and cannot be directly created')
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args, **kwargs):  # noqa
         raise NotImplementedError('This class is intended as a namespace and cannot be directly created')
